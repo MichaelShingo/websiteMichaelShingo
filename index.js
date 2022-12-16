8035808eb24d667c3bc53db23f19e8575f459051
MichaelShingo <mcrawford5376@gmail.com>
2022-12-16 13:03:53 -0500

    working on menu list items

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
+
+
+
+//Scroll progress bar
+
+
 const menuContainer = document.getElementById("menuContainer");
+const scrollProgress = document.getElementById("scrollProgress");
+window.onscroll = function() {scrollBarAnimation()};
+
+function scrollBarAnimation() {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+    document.getElementById('output').innerHTML = scrolled + "%";
+
+}
+
+
+
+//Hamburger menu
+const hamburgerLines = document.getElementsByClassName("hamburgerLine");
 const hamburgerContainer = document.getElementById("hamburgerContainer");
 
 hamburgerContainer.addEventListener("click", () => {
-    if (menuContainer.classList.contains("menuOpen")) {
-        
+    menuContainer.classList.toggle("menuOpen");
+    let i = 0;
+    for (let line of hamburgerLines){
+        if (i === 0){
+            line.classList.toggle("hamburger-x-top");
+        }
+        else if (i  === 4){
+            line.classList.toggle("hamburger-x-bottom");
+        }
+        else{
+            line.classList.toggle("invisible");
+        }
+        line.classList.toggle("hamburgerClose");
+        i++;
     }
-    else{
-        menuContainer.classList.toggle("menuOpen");
-    }
+    
 });
 
 
